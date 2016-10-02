28a806965d4afb091563c31e21fd5b87213b9def
mintos5 <michal.skuta@gmail.com>
2016-10-02 23:28:14 +0200

    Added JTabbedPane,device chooser

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -4,21 +4,70 @@ import model.Program;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by michal on 2.10.2016.
  */
 public class Main {
-
     private Program model;
     private JPanel prefab;
+    private JTabbedPane tabbedPane1;
+    private JComboBox comboBox1;
+    private JButton startButton;
+    private JTextPane textPane1;
+
+    public Main() {
+        startButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                comboBox1.getModel();
+            }
+        });
+
+    }
 
     public static void main(String[] args) {
+        Main mainko = new Main();
         JFrame frame = new JFrame("SwBridge");
-        frame.setContentPane(new Main().prefab);
+        frame.setContentPane(mainko.prefab);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setMinimumSize(new Dimension(600,300));
         frame.pack();
         frame.setVisible(true);
+    }
+
+    private void createUIComponents() {
+        model = new Program();
+        comboBox1 = new JComboBox(new DeviceComboBoxModel(model));
+        comboBox1.setSelectedIndex(0);
+    }
+}
+
+class DeviceComboBoxModel extends AbstractListModel implements ComboBoxModel{
+
+    Program model;
+    String selection;
+
+
+    public DeviceComboBoxModel(Program model) {
+        this.model = model;
+        this.model.getDevices();
+    }
+
+    public void setSelectedItem(Object o) {
+        selection = (String) o;
+    }
+
+    public Object getSelectedItem() {
+        return selection;
+    }
+
+    public int getSize() {
+        return model.getAlldevs().size();
+    }
+
+    public Object getElementAt(int i) {
+        return model.getAlldevs().get(i).getName();
     }
 }
--- src/main/java/model/Program.java
+++ src/main/java/model/Program.java
@@ -1,8 +1,33 @@
 package model;
 
+import org.jnetpcap.Pcap;
+import org.jnetpcap.PcapIf;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by michal on 2.10.2016.
  */
 public class Program {
 
+    List<PcapIf> alldevs;
+
+    public Program() {
+        alldevs = new ArrayList<PcapIf>();
+    }
+
+    public List<PcapIf> getAlldevs() {
+        return alldevs;
+    }
+
+    public void getDevices() {
+        StringBuilder errbuf = new StringBuilder(); // For any error msgs
+        int r = Pcap.findAllDevs(alldevs, errbuf);
+        if (r == Pcap.ERROR || alldevs.isEmpty()) {
+            System.err.printf("Can't read list of devices, error is %s", errbuf
+                    .toString());
+            return;
+        }
+    }
 }
